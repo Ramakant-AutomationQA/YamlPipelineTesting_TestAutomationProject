3c3b5a027bd3181dc5e9617ce77ae6ea28b9d0a1
dell <dell@DESKTOP-IJIHQ5I>
2024-08-17 23:22:31 +0530

    commit01

---
+++ YamlPipelineTesting_TestAutomationProject/src/main/java/Package01/ClassTestAutomation_A.java
@@ -0,0 +1,9 @@
+package Package01;
+
+public class ClassTestAutomation_A {
+
+	public static void main(String[] args) {
+		System.out.println("ClassTestAutomation_A====Simple_TestAutomation_Project01====01");
+	}
+
+}
