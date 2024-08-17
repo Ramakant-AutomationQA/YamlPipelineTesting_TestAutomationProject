7fdb8053f5c647b97ff90752d5744163f8113b75
dell <dell@DESKTOP-IJIHQ5I>
2024-08-17 23:23:11 +0530

    02

--- YamlPipelineTesting_TestAutomationProject/src/main/java/Package01/ClassTestAutomation_A.java
+++ YamlPipelineTesting_TestAutomationProject/src/main/java/Package01/ClassTestAutomation_A.java
@@ -3,7 +3,7 @@ package Package01;
 public class ClassTestAutomation_A {
 
 	public static void main(String[] args) {
-		System.out.println("ClassTestAutomation_A====Simple_TestAutomation_Project01====01");
+		System.out.println("ClassTestAutomation_A====Simple_TestAutomation_Project01====02");
 	}
 
 }
